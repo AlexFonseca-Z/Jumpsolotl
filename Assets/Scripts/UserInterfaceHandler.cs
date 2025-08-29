904744b7706a4b4dd6ef82bf475fb190c70d32c2
Alex <alex.fonseca.yw@gmail.com>
2025-08-29 16:10:52 +0100

    Update 1.1
    Added:
       - Last Score;
       - Best Score saving feature;
    
    Fixed:
       - Current Score showing even after player dies;

--- Assets/Scripts/UserInterfaceHandler.cs
+++ Assets/Scripts/UserInterfaceHandler.cs
@@ -10,13 +10,13 @@ public class UserInterfaceHandler : MonoBehaviour
     [SerializeField]
     private GameObject _menuUI; // Reference to the menuUI GameObject
     [SerializeField]    
-    private TextMeshProUGUI _scoreText, _bestScoreText; // Reference to the score TextMeshProUGUI component
+    private TextMeshProUGUI _scoreText, _bestScoreText, _lastScoreText; // Reference to the score TextMeshProUGUI component
     [SerializeField]
     private GameObject _scoreUI; // Reference to the score UI GameObject
 
 
     private PlayerControl _playerControl; // Reference to the PlayerControl script
-    private int _bestScore=0; // Player's best score
+    private int _bestScore=0, _lastScore=0; // Player's best score
 
 
     private void Start()
@@ -24,16 +24,33 @@ public class UserInterfaceHandler : MonoBehaviour
         _playerControl = _player.gameObject.GetComponent<PlayerControl>();
         _player.SetActive(false);  // Ensure the player is inactive at the start
         _scoreUI.SetActive(false); // Hide score UI at the start
+
+        _bestScore = PlayerPrefs.GetInt("BestScore", 0); // Load the best score from PlayerPrefs
+        _bestScoreText.text = _bestScore.ToString(); // Display the best score
     }
 
     private void Update()
     {
-        _scoreText.text = _playerControl.Score.ToString(); // Update the score display
-
-        if(_playerControl.Score > _bestScore)
+        if (_player.activeSelf && PlayerControl.GameStarted)
         {
-            _bestScore = _playerControl.Score; // Update the best score if the current score is higher
-            _bestScoreText.text = _bestScore.ToString(); // Update the best score display
+            _scoreText.text = _playerControl.Score.ToString(); // Update the score display
+
+            if (_playerControl.Score > _bestScore)
+            {
+                _bestScore = _playerControl.Score; // Update the best score if the current score is higher
+                _bestScoreText.text = _bestScore.ToString(); // Update the best score display
+                PlayerPrefs.SetInt("BestScore", _bestScore); // Save the new best score to PlayerPrefs
+                PlayerPrefs.Save(); // Ensure the best score is saved
+            }
+
+            if (_playerControl.Score > 0 && _playerControl.Score != _lastScore)
+            {
+                _lastScore = _playerControl.Score; // Update the last score when the player dies
+                _lastScoreText.text = _lastScore.ToString(); // Update the last score display
+            }
+        }else
+        {
+            _scoreUI.SetActive(false); // Hide score UI when the player is inactive
         }
     }
 
